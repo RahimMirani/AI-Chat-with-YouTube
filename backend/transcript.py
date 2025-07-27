6de4ab2305066b0cdba5e8c4eadb4dd60e4340ee
Abdul Rahim Mirani <abdulrahimmirani@gmail.com>
2025-07-27 02:13:31 -0400

    Fetching video transcripts accuratly

--- backend/transcript.py
+++ backend/transcript.py
@@ -1,22 +1,21 @@
-from youtube_transcript_api import YouTubeTranscriptApi, NoTranscriptFound
-
+from youtube_transcript_api import YouTubeTranscriptApi, NoTranscriptFound, TranscriptsDisabled
 
 def get_transcript(video_id: str):
-   
     try:
         transcript_initialize = YouTubeTranscriptApi()
-        transcript_list = transcript_initialize.fetch(video_id)
-        #transcript_text = " ".join([item['text'] for item in transcript_list])
-        return transcript_list
+        transcript_list = transcript_initialize.list(video_id)
+        transcript = transcript_list.find_transcript(['en', 'en-US'])
+        transcript_data = transcript.fetch()
+        return " ".join([item.text for item in transcript_data])
     
-    except NoTranscriptFound:
+    except (NoTranscriptFound, TranscriptsDisabled):
         print("No transcript found")
         return None
     
     except Exception as e:
         print(f"Error: {e}")
-        return None 
-    
+        return None
+
 
 Video = get_transcript("ydTdJDJMiKE")
 print(Video)
