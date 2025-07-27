192c02a1192ddbdf5e42d70158a2b8a899d31bd1
Abdul Rahim Mirani <abdulrahimmirani@gmail.com>
2025-07-26 21:39:56 -0400

    Debugging a youtube api error

--- backend/transcript.py
+++ backend/transcript.py
@@ -1,12 +1,32 @@
 from youtube_transcript_api import YouTubeTranscriptApi, NoTranscriptFound
 
+#print(dir(YouTubeTranscriptApi))
+
+
+video_id = "suakoHWghfg"
+Video1 = YouTubeTranscriptApi.get_transcript(video_id)
+
+print(Video1)
+
+
+"""
 def get_transcript(video_id: str):
    
     try:
         transcript_list = YouTubeTranscriptApi.get_transcript(video_id)
         transcript_text = " ".join([item['text'] for item in transcript_list])
         return transcript_text
+    
     except NoTranscriptFound:
+        print("No transcript found")
         return None
+    
     except Exception as e:
+        print(f"Error: {e}")
         return None 
+    
+
+video1 = get_transcript("ydTdJDJMiKE")
+print(video1)
+
+"""
