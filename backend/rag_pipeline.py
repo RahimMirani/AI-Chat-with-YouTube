6dd406fb1229c7a0b6f6e63ee73128c58ae19afc
Abdul Rahim Mirani <abdulrahimmirani@gmail.com>
2025-07-27 22:55:19 -0400

    Text slitting and vector stores

--- backend/rag_pipeline.py
+++ backend/rag_pipeline.py
@@ -1,6 +1,5 @@
 import os
 from dotenv import load_dotenv
-
 from langchain_community.vectorstores import FAISS
 from langchain_openai import OpenAIEmbeddings, ChatOpenAI
 from langchain.text_splitter import RecursiveCharacterTextSplitter
@@ -11,4 +10,16 @@ load_dotenv()
 class ChatPipeline:
     def __init__(self, transcript: str):
         self.transcript = transcript
-        self.qa_chain = None 
+        self.qa_chain = None
+
+    def build_vector_store(self):
+        text_splitter = RecursiveCharacterTextSplitter(
+            chunk_size=1000,
+            chunk_overlap=100
+        )
+        docs = text_splitter.split_text(self.transcript)
+        
+        embeddings = OpenAIEmbeddings()
+        
+        vector_store = FAISS.from_texts(docs, embeddings)
+        return vector_store 
