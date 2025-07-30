ccd0c5dc27a32d35c78d62a07d65caeee8e542ed
Abdul Rahim Mirani <abdulrahimmirani@gmail.com>
2025-07-30 18:45:48 -0400

    Modified the code to get raw youtube data

--- backend/transcript.py
+++ backend/transcript.py
@@ -6,11 +6,12 @@ def get_transcript(video_id: str):
         transcript_list = transcript_initialize.list(video_id)
         transcript = transcript_list.find_transcript(['en', 'en-US'])
         transcript_data_raw = transcript.fetch()
-        transcript_filtered = " ".join([item.text for item in transcript_data_raw])
-        return transcript_filtered
+        # transcript_filtered = " ".join([item.text for item in transcript_data_raw])
+        # return transcript_filtered
+        return transcript_data_raw
     
     except (NoTranscriptFound, TranscriptsDisabled):
-        return ("No transcript found")
+        return "No transcript found"
     
     except Exception as e:
         return (f"Error: {e}")
