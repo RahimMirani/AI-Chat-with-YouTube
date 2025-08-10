6a43f82051e1e76ed8604810bbf6410a829bab09
Abdul Rahim Mirani <abdulrahimmirani@gmail.com>
2025-08-10 12:21:07 -0400

    Added cors middleware so the backend can handle all the types of requests

--- backend/main.py
+++ backend/main.py
@@ -1,10 +1,20 @@
 from fastapi import FastAPI, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 
 from transcript import get_transcript
 from rag_pipeline import ChatPipeline
 
 app = FastAPI()
+
+# --- Add CORS Middleware ---
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"], # Allows all origins
+    allow_credentials=True,
+    allow_methods=["*"], # Allows all methods
+    allow_headers=["*"], # Allows all headers
+)
 
 class ChatRequest(BaseModel):
     video_id: str
